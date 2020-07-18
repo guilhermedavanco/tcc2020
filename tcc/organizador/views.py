3aeff2a4b395ec740bb971fcb4393180792f637c
guilhermedavanco <guilhermedavanco@yahoo.com.br>
2020-07-17 21:05:06 -0300

    site evoluindo

--- tcc/organizador/views.py
+++ tcc/organizador/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Item,Fluxo
+from .form import addItem
 
 # Create your views here.
 
 def selecao(request):
 	return render(request,'organizador/Selecao.html')
 def superior(request):
-	return render(request,'organizador/Selecao.html')
+	pecas=Item.objects.filter(tipo="S")
+	print(pecas)
+	return render(request,'organizador/Superior.html',{'pecas':pecas})
 def inferior(request):
-	return render(request,'organizador/Selecao.html')
+	pecas=Item.objects.filter(tipo="I")
+	print(pecas)
+
+	return render(request,'organizador/Inferior.html',{'pecas':pecas})
 def add(request):
-	return render(request,'organizador/Selecao.html')
+	if request.method == "POST":
+		print('entrou!')
+		form = addItem(request.POST, request.FILES)
+		print(form)
+		if form.is_valid():
+			form.save()
+			print('salvo')
+			return redirect('superior/')
+		else:
+			print('nao entrou')
+	else:
+		form= addItem()
+	return render(request,'organizador/Add.html',{'form':form})
 def favoritos(request):
-	return render(request,'organizador/Selecao.html')
+	return render(request,'organizador/Favoritos.html')
