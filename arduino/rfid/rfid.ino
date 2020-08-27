8fb4b70aa17b0d70351b9e23225178cc3948b114
guilhermedavanco <guilhermedavanco@yahoo.com.br>
2020-08-26 21:57:35 -0300

    RFID VOANDO

--- arduino/rfid/rfid.ino
+++ arduino/rfid/rfid.ino
@@ -1,58 +1,99 @@
-#include <SPI.h>
-#include <MFRC522.h>
+/**************************************************************************/
+/*! 
+    This example will attempt to connect to an ISO14443A
+    card or tag and retrieve some basic information about it
+    that can be used to determine what type of card it is.   
+   
+    Note that you need the baud rate to be 115200 because we need to print
+    out the data and read from the card at the same time!
 
-#define SS_PIN 10
-#define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+    To enable debug message, define DEBUG in PN532/PN532_debug.h
+    
+*/
+/**************************************************************************/
 
-char st[20];
 
-void setup()
-{
-  Serial.begin(9600);   // Inicia a serial
-  SPI.begin();      // Inicia  SPI bus
-  mfrc522.PCD_Init();   // Inicia MFRC522
-  Serial.println("Aproxime o seu cartao do leitor...");
-  Serial.println();
+/* When the number after #if set as 1, it will be switch to SPI Mode*/
+#if 0
+  #include <SPI.h>
+  #include <PN532_SPI.h>
+  #include "PN532.h"
+
+  PN532_SPI pn532spi(SPI, 10);
+  PN532 nfc(pn532spi);
+
+/* When the number after #elif set as 1, it will be switch to HSU Mode*/
+#elif 0
+  #include <PN532_HSU.h>
+  #include <PN532.h>
+      
+  PN532_HSU pn532hsu(Serial1);
+  PN532 nfc(pn532hsu);
+
+/* When the number after #if & #elif set as 0, it will be switch to I2C Mode*/
+#else 
+  #include <Wire.h>
+  #include <PN532_I2C.h>
+  #include <PN532.h>
+  #include <NfcAdapter.h>
+  
+  PN532_I2C pn532i2c(Wire);
+  PN532 nfc(pn532i2c);
+#endif
+  
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  nfc.setPassiveActivationRetries(0xFF);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+    
+  Serial.println("Waiting for an ISO14443A card");
 }
 
-void loop()
-{
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
+void loop(void) {
+  boolean success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+  
+  if (success) {
+    Serial.println("Found a card!");
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    for (uint8_t i=0; i < uidLength; i++) 
+    {
+      Serial.print(" 0x");Serial.print(uid[i], HEX); 
+    }
+    Serial.println("");
+    // Wait 1 second before continuing
+    delay(1000);
   }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial())
+  else
   {
-    return;
-  }
-  //Mostra UID na serial
-  Serial.print("UID da tag :");
-  String conteudo = "";
-  byte letra;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Mensagem : ");
-  conteudo.toUpperCase();
-  if (conteudo.substring(1) == "ED 78 03 CA") //UID 1 - Chaveiro
-  {
-    Serial.println("Ola FILIPEFLOP !");
-    Serial.println();
-    delay(3000);
-  }
-
-  if (conteudo.substring(1) == "BD 9B 06 7D") //UID 2 - Cartao
-  {
-    Serial.println("Ola Cartao !");
-    Serial.println();
-    delay(3000);
+    // PN532 probably timed out waiting for a card
+    Serial.println("Timed out waiting for a card");
   }
 }
