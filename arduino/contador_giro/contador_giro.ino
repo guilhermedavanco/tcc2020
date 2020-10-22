97bd96adb340c06eab4e142ebe8e12606e9faadf
Vinnicius Souza <15.00437-6@maua.br>
2020-10-22 11:25:12 -0300

    Adicionado debounce no programa teste de giro

--- arduino/contador_giro/contador_giro.ino
+++ arduino/contador_giro/contador_giro.ino
@@ -1,21 +1,35 @@
-#define REED 21 //input do reed switch
+#define REED 20//input do reed switch
 #define INTERVALO_MINIMO 250  //intervalo entre dois pulsos para reset
 #define NUM_POS 20  //número de posições do armário
 #define PIN_MOTOR 8
 
 int pos = 0;
-long int intervaloInterrupt = 0;
+unsigned long ultimoInterrupt = 0;
+int posDesejada = 0;
 
 void setup() {
   pinMode(PIN_MOTOR, OUTPUT);
-  digitalWrite(PIN_MOTOR, HIGH);
+  digitalWrite(PIN_MOTOR, LOW);
   attachInterrupt(digitalPinToInterrupt(REED), incrementarPosicao, RISING);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(pos);
-  if (pos == 10) {
+  char c;
+  char dados[8];
+  int x = 0;
+  if (Serial.available()) {
+    do {
+      c = Serial1.read();
+      dados[x] = c;
+      x++;
+      delay(1);
+    } while (c != '\n');
+    dados[x - 1] = '\0';
+    Serial.println(dados);
+    posDesejada = atoi(dados);
+  }
+  if (pos == posDesejada) {
     digitalWrite(PIN_MOTOR, LOW);
   }
   else {
@@ -25,9 +39,10 @@ void loop() {
 }
 
 void incrementarPosicao() {
-  pos++;
-  intervaloInterrupt = millis() - intervaloInterrupt;
-  if (pos > NUM_POS || intervaloInterrupt < INTERVALO_MINIMO) {
-    pos = 0;
+  if (millis() - ultimoInterrupt  > 250) {
+    pos++;
+    pos = pos % NUM_POS;
+    Serial.println(pos);
+    ultimoInterrupt = millis();
   }
 }
